ab6f225a818ec146507625173e51c27806bcd4af
Kajetan Duszyński <kajetanduszynski@gmail.com>
2025-10-17 11:11:31 +0200

    Initial with first models

--- copilot-pr-demo-test1/copilot-pr-demo-test1/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace copilot_pr_demo_test1.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
